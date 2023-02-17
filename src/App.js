73b865f50a96264bbd55596a3f05462a0ed185ec
abraham <abrahamsuarezolay@gmail.com>
2023-02-17 18:20:16 +0000

    First data checks between our components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { nanoid } from 'nanoid';
 import NoteList from "./components/NoteList";
 
 
 const App = () => {
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: "This is my first note!",
+    date: "17/02/2023"
+  },
+  {
+    id: nanoid(),
+    text: "This is my second note!",
+    date: "17/02/2023"
+  },
+  {
+    id: nanoid(),
+    text: "This is my third note!",
+    date: "17/02/2023"
+  }
+]);
   return (
   <div className="container">
-    <NoteList/>
+    <NoteList notes={notes}/>
 
   </div>);
 };
